199b2f4e6dcdd71d2624903147b5dd75216db2e9
Varuna Priya <varunapriya93@gmail.com>
2025-12-07 22:31:47 +0530

    Updated message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CI/CD working successfully! 🚀"
+    return "Updated successfully via CI/CD 🎯"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
